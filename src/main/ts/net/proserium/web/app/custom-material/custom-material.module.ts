5bad8a6d5ddd5314c27e6d00c00d185257d4d592
micmine <micmine4@gmail.com>
2019-09-30 10:18:56 +0200

    Add gradlew lint

--- src/main/ts/net/proserium/web/app/custom-material/custom-material.module.ts
+++ src/main/ts/net/proserium/web/app/custom-material/custom-material.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule, MatMenuModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatTooltipModule, MatSliderModule } from '@angular/material';
-import { MatIconModule, MatCardModule, MatDividerModule, MatStepperModule, MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { MatToolbarModule, MatMenuModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatTooltipModule } from '@angular/material';
+import { MatIconModule, MatCardModule, MatDividerModule, MatStepperModule, MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatSliderModule } from '@angular/material';
 import { MatSlideToggleModule} from '@angular/material';
 import { ReactiveFormsModule } from '@angular/forms';
 
